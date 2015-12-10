459cd65aa51ca5af055b6a3e80c2090934ba066f
Stephane Demotte <stephanedemotte@iMac-de-Stephane.local>
2015-12-10 13:46:34 -0500

    fix

--- index.js
+++ index.js
@@ -3,17 +3,11 @@ var _        = require('underscore');
 var Elixir   = require('laravel-elixir');
 var sprity	 = require('sprity');
 var gulpif   = require('gulp-if');
-var config   = Elixir.config;
 
 Elixir.extend('sprity', function(options) {
 
-    config.images = _.extend({
-			src: config.assetsPath + '/sprites',
-			output: config.assetsPath + '/img',
-    }, config.images || {});
-
 		options = _.extend({
-			src: config.images.src + '/**/*.{png,jpg,jpeg}',
+			src: config.assetsPath + '/sprites/**/*.{png,jpg,jpeg}',
 			name: 'sprites',
 			style: 'Sprites.styl',
 			cssPath: '../img/',
@@ -25,19 +19,21 @@ Elixir.extend('sprity', function(options) {
 				}, {
 					ratio: 2, dpi: 192
 				}],
-			outputMixin: config.assetsPath + '/stylus/mixins'
+			outputMixin: config.assetsPath + '/stylus/mixins',
+			outputSprites: config.publicPath + '/img'
 		}, options);
 
     new Elixir.Task('sprity', function () {
 
 			return sprity.src(options)
 			.pipe(gulpif('*.png', 
-				gulp.dest(config.images.output), 
-				gulp.dest(options.outputMixin || config.assetsPath + '/stylus/mixins')
+				gulp.dest(options.outputSprites), 
+				gulp.dest(options.outputMixin)
 			))
 			.pipe(new Elixir.Notification('Sprity Complete!'))
 
     })
-    .watch(config.images.src + '/**/*.{png,jpg,jpeg}')
+    .watch(options.src)
 });
+
 
